cbea10fd817a94b5e8bca14da4e3ca11d22c565d
Nicholas Morey <nicholas@morey.tech>
2023-03-26 12:10:58 -0400

    ci: fix name of test actions

--- src/argocd/AppCollection.ts
+++ src/argocd/AppCollection.ts
@@ -4,7 +4,7 @@ export class AppCollection {
   apps: App[];
 
   constructor(apps: App[]) {
-    this.apps = apps;  // AppCollection created with null apps value?
+    this.apps = apps; // AppCollection created with null apps value?
   }
 
   filterByExcludedPath(excludedPaths: string[]): AppCollection {
@@ -21,7 +21,8 @@ export class AppCollection {
 
   filterByRepo(repoMatch: string): AppCollection {
     return new AppCollection(
-      this.apps.filter(app => {  // Then trys to filter on the null apps?
+      this.apps.filter(app => {
+        // Then trys to filter on the null apps?
         console.log(app);
         return app.spec.source.repoURL.includes(repoMatch);
       })
